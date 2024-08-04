b80525773b401377c9b69d0432ac6e9b00666932
mo genedy <mogenedy2030@gmail.com>
2024-08-04 19:51:31 +0300

    fix name

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,7 +6,7 @@ use App\Models\Invoice;
 
 class AdminController extends Controller
 {
-   public function admin_dashboard()
+   public function adminDashboard()
    {
       $invoices = Invoice::all();
       return view('admin.index', compact('invoices'));
